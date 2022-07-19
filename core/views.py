7d1fcab16395189da8862ef172ede8a9031fe5f5
Haoyu Lu <luhaoyu21@hotmail.com>
2022-07-19 17:12:55 -0400

    change the templates
    change the templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from django.http.response import StreamingHttpResponse
+from django.template import RequestContext, loader
+import os
+import sys
+import time
 
-# Create your views here.
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render({}, request))
